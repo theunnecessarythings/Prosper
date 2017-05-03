580a2ab1d23de32a68f2dcb47d4d30be4948fc50
Sreeraj R <sreerajr000@gmail.com>
2017-05-03 23:47:29 +0530

    Update action.php

--- action.php
+++ action.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Welcome <?php echo $_GET["firstname"]; ?><br>
+Your email address is: <?php echo $_GET["lastname"]; ?>
 
 </body>
 </html> 
